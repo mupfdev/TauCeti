fd4c1c5d64a0371d15142fab42b76abafe3b7f9e
Michael Fitzmayer <mail@michael-fitzmayer.de>
2020-07-06 23:40:36 +0200

    Engine rewrite: backup current progress

--- src/main.c
+++ src/main.c
@@ -11,32 +11,43 @@
 #include <SDL.h>
 #include <esz.h>
 
-static void CoreStartedCallback_(void* core);
-static void KeyDownCallback_(void* core);
+static void key_down_callback_1(void* window, void* core);
 
 int main()
 {
-    esz_Status status = ESZ_OK;
-    esz_Core*  core   = NULL;
-    esz_Config config = { 640, 360, 384, 216, SDL_FALSE };
+    esz_Status       status = ESZ_OK;
+    esz_Window*      window = NULL;
+    esz_WindowConfig config = { 640, 360, 384, 216, SDL_FALSE };
+    esz_Core*        core_1 = NULL;
 
-    status = esz_InitCore(&config, &core);
+    status = esz_CreateWindow("Tau Ceti", &config, &window);
+
+    status = esz_InitCore(&core_1);
     if (ESZ_OK != status)
     {
         goto quit;
     }
 
-    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
-    esz_RegisterEventCallback(EVENT_CORE_STARTED, &CoreStartedCallback_, (void*)core);
-    esz_RegisterEventCallback(EVENT_KEYDOWN,      &KeyDownCallback_,     (void*)core);
+    esz_LoadMap("res/maps/city.tmx", window, core_1);
 
-    status = esz_StartCore("Tau Ceti", core);
-    if (ESZ_OK != status)
+    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
+    esz_RegisterEventCallback(EVENT_KEYDOWN, &key_down_callback_1, (void*)core_1);
+
+    while (esz_IsCoreActive(core_1))
     {
-        goto quit;
+        Uint32 time_a = 0;
+        Uint32 time_b = 0;
+
+        esz_UpdateCore(window, core_1);
+
+        esz_DrawFrame(&time_a, &time_b, window, core_1);
     }
 
 quit:
+    esz_UnloadMap(window, core_1);
+    esz_DestroyCore(core_1);
+    esz_DestroyWindow(window);
+
     if (ESZ_OK != status)
     {
         return EXIT_FAILURE;
@@ -45,26 +56,20 @@ quit:
     return EXIT_SUCCESS;
 }
 
-static void CoreStartedCallback_(void* core)
-{
-    esz_LoadMap("res/maps/city.tmx", core);
-}
-
-static void KeyDownCallback_(void* core)
+static void key_down_callback_1(void* window, void* core)
 {
     switch (esz_GetKeycode(core))
     {
         case SDLK_f:
-            esz_ToggleFullscreen(core);
+            esz_ToggleFullscreen(window);
             break;
         case SDLK_q:
-            esz_ExitCore(core);
+            esz_DeactivateCore(core);
             break;
         case SDLK_F4:
-            esz_LoadMap("res/maps/city.tmx", core);
+            esz_LoadMap("res/maps/city.tmx", window, core);
             break;
         case SDLK_F5:
-            esz_UnloadMap(core);
-            break;
+            esz_UnloadMap(window, core);
     }
 }
